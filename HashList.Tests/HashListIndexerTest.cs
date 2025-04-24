6351c46a6a890aa3830df12d3934a6afb97574ae
Fallever <piscovip@163.com>
2025-04-24 16:42:09 +0800

    M 第一版HashList.Indexer
    Signed-off-by: Fallever <piscovip@163.com>

--- HashList.Tests/HashListIndexerTest.cs
+++ HashList.Tests/HashListIndexerTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using Mio.Collections.HashList;
 
@@ -16,17 +17,19 @@ public class HashListIndexerTest
         public int phone { get; set; }
     }
 
-    public List<Person> FivePersonList = new List<Person>(){
-        new Person(){Id = 1, Name = "张三", phone = 123451},
-        new Person(){Id = 2, Name = "李四", phone = 123452},
-        new Person(){Id = 3, Name = "王五", phone = 123453},
-        new Person(){Id = 4, Name = "赵六", phone = 123454},
-        new Person(){Id = 5, Name = "孙七", phone = 123455},
-    };
+    public List<Person> FivePersonList;
 
     [SetUp]
     public void Setup()
     {
+        FivePersonList = new List<Person>()
+        {
+            new Person(){Id = 1, Name = "张三", phone = 123451},
+            new Person(){Id = 2, Name = "李四", phone = 123452},
+            new Person(){Id = 3, Name = "王五", phone = 123453},
+            new Person(){Id = 4, Name = "赵六", phone = 123454},
+            new Person(){Id = 5, Name = "孙七", phone = 123455},
+        };
     }
 
     public IList<TValue> GetList<TValue>(IHashListIndexer<TValue> target)
@@ -89,11 +92,15 @@ public class HashListIndexerTest
         Assert.That(indexer.Remove(new Person() { Id = 1, Name = "张三", phone = 123456 }), Is.False);
         Assert.That(GetList(indexer), Has.Count.EqualTo(4));
         CheckContainer(indexer);
+    }
 
+    public void TestRemoveItemSelfItem()
+    {
         var indexer2 = new HashListIndexer<Person, int>(FivePersonList, x => x.Id);
-        var p = GetList
-        Assert.That(indexer2.Remove(), Is.False);
-        Assert.That(GetList(indexer2), Has.Count.EqualTo(5));
+        var p = GetList(indexer2)[0];
+        Assert.That(indexer2.Remove(p), Is.True);
+        Assert.That(indexer2.Remove(p), Is.False);
+        Assert.That(GetList(indexer2), Has.Count.EqualTo(4));
         CheckContainer(indexer2);
     }
 
@@ -104,15 +111,130 @@ public class HashListIndexerTest
         Assert.That(indexer.Remove(1), Is.True);
         Assert.That(indexer.Remove(1), Is.False);
         Assert.That(GetList(indexer), Has.Count.EqualTo(4));
-        CheckContainer(indexer);
-
-        var indexer2 = new HashListIndexer<Person, int>(FivePersonList, x => x.Id);
-        Assert.That(indexer2.Remove(6), Is.False);
-        Assert.That(GetList(indexer2), Has.Count.EqualTo(5));
-        CheckContainer(indexer2);
+        IList<Person> list = GetList(indexer);
+        for (int i = 0; i < list.Count; i++)
+        {
+            Person p = list[i];
+            TestContext.Out.WriteLine($"{p.Id} {p.Name} {p.phone}");
+            Assert.That(p.Id, Is.EqualTo(i + 2));
+        }
     }
 
-    
+    [Test]
+    public void TestRemoveAt()
+    {
+        var indexer = new HashListIndexer<Person, int>(FivePersonList, x => x.Id);
+        indexer.RemoveAt(0);
+        Assert.That(GetList(indexer), Has.Count.EqualTo(4));
+
+    }
+
+    [Test]
+    public void TestRemoveAtOutRange()
+    {
+        var indexer = new HashListIndexer<Person, int>(FivePersonList, x => x.Id);
+        Assert.That(() => indexer.RemoveAt(6), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        Assert.That(GetList(indexer), Has.Count.EqualTo(5));
+        CheckContainer(indexer);
+    }
+
+    [Test]
+    public void TestRemoveRange()
+    {
+        var indexer = new HashListIndexer<Person, int>(FivePersonList, x => x.Id);
+        indexer.RemoveRange(0, 2);
+        Assert.That(GetList(indexer), Has.Count.EqualTo(3));
+        CheckContainer(indexer);
+    }
+
+    [Test]
+    public void TestRemoveRangeOutOfRange()
+    {
+        var indexer = new HashListIndexer<Person, int>(FivePersonList, x => x.Id);
+        Assert.That(() => indexer.RemoveRange(0, 6), Throws.Exception.TypeOf<IndexOutOfRangeException>());
+        Assert.That(() => indexer.RemoveRange(6, 0), Throws.Exception.TypeOf<IndexOutOfRangeException>());
+        Assert.That(() => indexer.RemoveRange(6, 6), Throws.Exception.TypeOf<IndexOutOfRangeException>());
+        Assert.That(GetList(indexer), Has.Count.EqualTo(5));
+        CheckContainer(indexer);
+    }
+
+    [Test]
+    public void TestRemoveAll()
+    {
+        var indexer = new HashListIndexer<Person, int>(FivePersonList, x => x.Id);
+        indexer.RemoveAll(x => x.Id % 2 == 0);
+        Assert.That(GetList(indexer), Has.Count.EqualTo(3));
+        CheckContainer(indexer);
+    }
+
+    [Test]
+    public void TestRemoveAllNull()
+    {
+        var indexer = new HashListIndexer<Person, int>(FivePersonList, x => x.Id);
+        Assert.That(() => indexer.RemoveAll(null), Throws.ArgumentNullException);
+        Assert.That(GetList(indexer), Has.Count.EqualTo(5));
+        CheckContainer(indexer);
+    }
+
+    [Test]
+    public void TestInsert()
+    {
+        var indexer = new HashListIndexer<Person, int>(FivePersonList, x => x.Id);
+        var newPerson = new Person() { Id = 6, Name = "周八", phone = 123456 };
+        indexer.Insert(0, newPerson);
+        Assert.That(GetList(indexer)[0], Is.EqualTo(newPerson));
+        Assert.That(GetList(indexer), Has.Count.EqualTo(6));
+        CheckContainer(indexer);
+    }
+
+    [Test]
+    public void TestInsertOutOfRange()
+    {
+        var indexer = new HashListIndexer<Person, int>(FivePersonList, x => x.Id);
+        var newPerson = new Person() { Id = 6, Name = "周八", phone = 123456 };
+        Assert.That(() => indexer.Insert(-1, newPerson), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        Assert.That(() => indexer.Insert(6, newPerson), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        Assert.That(GetList(indexer), Has.Count.EqualTo(5));
+        CheckContainer(indexer);
+    }
+
+    [Test]
+    public void TestInsertRange()
+    {
+        var indexer = new HashListIndexer<Person, int>(FivePersonList, x => x.Id);
+        var newPersons = new List<Person>()
+        {
+            new Person() { Id = 6, Name = "周八", phone = 123456 },
+            new Person() { Id = 7, Name = "吴九", phone = 123457 }
+        };
+        indexer.InsertRange(0, newPersons);
+        Assert.That(GetList(indexer)[0], Is.EqualTo(newPersons[0]));
+        Assert.That(GetList(indexer)[1], Is.EqualTo(newPersons[1]));
+        Assert.That(GetList(indexer), Has.Count.EqualTo(7));
+        CheckContainer(indexer);
+    }
+
+    [Test]
+    public void TestContains()
+    {
+        var indexer = new HashListIndexer<Person, int>(FivePersonList, x => x.Id);
+        Assert.That(indexer.Contains(new Person() { Id = 1, Name = "张三", phone = 123451 }), Is.False);
+        Assert.That(indexer.Contains(new Person() { Id = 6, Name = "周八", phone = 123456 }), Is.False);
+        var p = GetList(indexer)[0];
+        Assert.That(indexer.Contains(p), Is.True);
+        Assert.That(GetList(indexer), Has.Count.EqualTo(5));
+        CheckContainer(indexer);
+    }
+
+    [Test]
+    public void TestClear()
+    {
+        var indexer = new HashListIndexer<Person, int>(FivePersonList, x => x.Id);
+        indexer.Clear();
+        Assert.That(GetList(indexer), Has.Count.EqualTo(0));
+        Assert.That(GetIndex<int, Person>(indexer), Has.Count.EqualTo(0));
+    }
+
     void CheckContainer(HashListIndexer<Person, int> indexer)
     {
         var list = GetList(indexer);
--- HashList/HashList.Indexer.cs
+++ HashList/HashList.Indexer.cs
@@ -10,19 +10,7 @@ namespace Mio.Collections.HashList;
 public interface IHashListIndexer<TElement>
 {
     public bool Add(TElement element);
-
-    /// <summary>
-    /// 添加指定集合的全部元素,如果有重复元素则跳过
-    /// </summary>
-    /// <param name="elements"></param>
-    /// <returns>所有元素都成功添加才返回true</returns>
     public bool AddRange(IEnumerable<TElement> elements);
-
-    /// <summary>
-    /// 按元素的key移除元素,该元素可以不是list里的item,只要对应的key存在就会移除
-    /// </summary>
-    /// <param name="element"></param>
-    /// <returns></returns>
     public bool Remove(TElement element);
     // public bool RemoveRange(IEnumerable(TElement))
     public void Clear();
@@ -32,20 +20,9 @@ public sealed class HashListIndexer<TElement, TKey> : IHashListIndexer<TElement>
 {
     private readonly Func<TElement, TKey> _keySelector;
     private readonly Dictionary<TKey, TElement> _indexer;
-    private readonly IList<TElement> _list;
+    private readonly List<TElement> _list;
 
-    public HashListIndexer(Func<TElement, TKey> keySelector)
-    {
-
-    }
-
-    /// <summary>
-    /// 通过外部列表构建索引器
-    /// </summary>
-    /// <param name="list"></param>
-    /// <param name="keySelector"></param>
-    /// <exception cref="ArgumentNullException"></exception>
-    public HashListIndexer(IList<TElement> list, Func<TElement, TKey> keySelector)
+    public HashListIndexer(List<TElement> list, Func<TElement, TKey> keySelector)
     {
         if (list == null) throw new ArgumentNullException(nameof(list));
         if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
@@ -55,8 +32,42 @@ public sealed class HashListIndexer<TElement, TKey> : IHashListIndexer<TElement>
         _indexer = new Dictionary<TKey, TElement>(list.Select(x => KeyValuePair.Create(keySelector(x), x)));
     }
 
-    public TElement this[TKey key] => _indexer[key];
+    public TElement this[TKey key]
+    {
+        get
+        {
+            if (_indexer.TryGetValue(key, out var result))
+                return result;
+            return default;
+        }
+        set
+        {
+            if (_indexer.TryGetValue(key, out var result))
+                _list[_list.IndexOf(result)] = value;
+            else
+                _list.Add(value);
+            _indexer[key] = value;
+        }
+    }
+
     public int Count => _indexer.Count;
+
+    /// <summary>
+    /// 检查索引器是否有效,即索引器和列表的元素是否一致
+    /// </summary>
+    public bool IsValid()
+    {
+        if (_list.Count != _indexer.Count)
+            return false;
+
+        foreach (var item in _list)
+        {
+            var key = _keySelector(item);
+            if (!_indexer.TryGetValue(key, out var value) || !EqualityComparer<TElement>.Default.Equals(value, item))
+                return false;
+        }
+        return true;
+    }
 
     public bool Add(TElement element)
     {
@@ -72,6 +83,13 @@ public sealed class HashListIndexer<TElement, TKey> : IHashListIndexer<TElement>
             return false;
     }
 
+    /// <summary>
+    /// 通过外部列表构建索引器
+    /// </summary>
+    /// <remarks>注意这样做不能再通过列表接口去增删元素</remarks>
+    /// <param name="list"></param>
+    /// <param name="keySelector"></param>
+    /// <exception cref="ArgumentNullException"></exception>
     public bool AddRange(IEnumerable<TElement> collection)
     {
         if (collection == null) throw new ArgumentNullException(nameof(collection));
@@ -82,60 +100,74 @@ public sealed class HashListIndexer<TElement, TKey> : IHashListIndexer<TElement>
         return result;
     }
 
+    /// <summary>
+    /// 按元素的key移除元素,该元素可以不是list里的item,只要对应的key存在就会移除
+    /// </summary>
+    /// <param name="element"></param>
+    /// <returns></returns>
     public bool Remove(TElement element)
     {
         if (element == null) throw new ArgumentNullException(nameof(element));
 
-        var key = _keySelector(element);
-        if (_indexer.Remove(key, out element)) // 不是list里的item,也会被移除
-        {
-            _list.Remove(element);
-            return true;
-        }
-        else
-            return false;
+        var key = _keySelector(element); // 不是list里的item,也会被移除
+        return Remove(key); // WARNING 遍历消耗 见下面
     }
 
+    /// <summary>
+    /// 按key移除元素
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
     public bool Remove(TKey key)
     {
         if (_indexer.Remove(key, out var element))
         {
-            _list.Remove(element);
+            _list.Remove(element); // WARNING 这儿有遍历消耗
             return true;
         }
         else
             return false;
     }
 
-    public bool RemoveAt(int index)
+    /// <summary>
+    /// 按索引移除元素
+    /// </summary>
+    /// <param name="index"></param>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public void RemoveAt(int index)
     {
         if (index < 0 || index >= _list.Count) throw new ArgumentOutOfRangeException(nameof(index));
-
         var element = _list[index];
-        return Remove(element);
+        _list.RemoveAt(index);
+        _indexer.Remove(_keySelector(element));
     }
 
     public void RemoveRange(int index, int count)
     {
-        if (index < 0 || index >= _list.Count) throw new ArgumentOutOfRangeException(nameof(index));
-        if (count < 0 || index + count > _list.Count) throw new ArgumentOutOfRangeException(nameof(count));
+        if (index < 0) throw new IndexOutOfRangeException(nameof(index));
+        if (count < 0) throw new IndexOutOfRangeException(nameof(count));
+        if (_list.Count - index < count) throw new IndexOutOfRangeException($"Remove index count out of range index:{index} count:{count}");
 
-        for (int i = index; i < index + count; i++)
+        for (int i = index + count - 1; i >= index; i--)
         {
             var element = _list[i];
-            Remove(element);
+            _indexer.Remove(_keySelector(element));
         }
+        _list.RemoveRange(index, count);
     }
 
     public void RemoveAll(Predicate<TElement> match)
     {
         if (match == null) throw new ArgumentNullException(nameof(match));
 
-        for (int i = 0; i < _list.Count; i++)
+        for (int i = _list.Count - 1; i >= 0; i--)
         {
             var element = _list[i];
             if (match(element))
-                Remove(element);
+            {
+                _list.RemoveAt(i);
+                _indexer.Remove(_keySelector(element));
+            }
         }
     }
 
@@ -171,16 +203,17 @@ public sealed class HashListIndexer<TElement, TKey> : IHashListIndexer<TElement>
         return result;
     }
 
-    public bool Contains(TElement element)
+    public bool Contains(TElement item)
     {
-        if (element == null) throw new ArgumentNullException(nameof(element));
+        if (item == null) throw new ArgumentNullException(nameof(item));
 
-        var key = _keySelector(element);
-        return _indexer.ContainsKey(key);
+        var key = _keySelector(item);
+        return _indexer.TryGetValue(key, out var e) && EqualityComparer<TElement>.Default.Equals(e, item);
     }
 
     public void Clear()
     {
+        _list.Clear();
         _indexer.Clear();
     }
 }
